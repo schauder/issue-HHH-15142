5af8631cf9a8eb391f377600bf7390c54d340922
Jens Schauder <jschauder@vmware.com>
2022-03-25 10:29:25 +0100

    Reproduce issue

--- src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -3,10 +3,19 @@ package org.hibernate.bugs;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -32,6 +41,33 @@ public class JPAUnitTestCase {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 		// Do stuff...
+		entityManager.persist(new Person("Person 1"));
+		entityManager.persist(new Person("Person 2"));
+
+
+		final CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		final CriteriaQuery<Person> personQuery = cb.createQuery(Person.class);
+		final Root<Person> root = personQuery.from(Person.class);
+		final ParameterExpression<String> pattern = cb.parameter(String.class);
+		CriteriaQuery<Person> criteriaQuery = personQuery
+				.where(cb.like(
+						root.get("name"),
+						pattern,
+						cb.literal('\\')
+				));
+
+
+		for (int i = 0; i < 2; i++) {
+			System.out.println("Iteration: " + i);
+
+			final TypedQuery<Person> query = entityManager.createQuery(criteriaQuery); //  <--- This fails when executed for the second time.
+			query.setParameter(pattern, "%_1");
+			final List<Person> result = query.getResultList();
+
+			assertThat(result).hasSize(1);
+		}
+
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
